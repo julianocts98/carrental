0538d976a66b0016280bc3cfdc1892eee0c1d9d9
Juliano Cavalcante Teixeira da Silva <juliano.cavalcante98@gmail.com>
2022-05-01 12:01:26 -0300

    Add GUI for creating specifications

---
+++ src/main/java/com/juliano/carrental/gui/CreateSpecificationBtnActionListener.java
@@ -0,0 +1,54 @@
+package com.juliano.carrental.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import com.juliano.carrental.dao.DaoFactory;
+import com.juliano.carrental.model.Specification;
+
+public class CreateSpecificationBtnActionListener implements ActionListener {
+    private final DaoFactory daoFactory;
+    private final JTextField nameTextField;
+    private final JTextField descriptionTextField;
+    private final JFrame frame;
+
+    public CreateSpecificationBtnActionListener(JFrame frame, DaoFactory daoFactory,
+            JTextField nameTextField,
+            JTextField descriptionTextField) {
+        this.frame = frame;
+        this.daoFactory = daoFactory;
+        this.nameTextField = nameTextField;
+        this.descriptionTextField = descriptionTextField;
+    }
+
+    private void clearTextFields() {
+        this.nameTextField.setText("");
+        this.descriptionTextField.setText("");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Specification specification = new Specification();
+        specification.setName(this.nameTextField.getText());
+        specification.setDescription(this.descriptionTextField.getText());
+
+        if (!daoFactory.getSpecificationDao().save(specification)) {
+            String errMessage = String.format("A especificação %s já existe!",
+                    this.nameTextField.getText());
+            JOptionPane.showMessageDialog(this.frame, errMessage, "ERRO",
+                    JOptionPane.ERROR_MESSAGE);
+            nameTextField.grabFocus();
+            return;
+        }
+        String successMessage = String.format("A especificação %s foi cadastrada com sucesso!",
+                this.nameTextField.getText());
+        JOptionPane.showMessageDialog(this.frame, successMessage, "SUCESSO",
+                JOptionPane.PLAIN_MESSAGE);
+        this.clearTextFields();
+        nameTextField.grabFocus();
+    }
+}
---
+++ src/main/java/com/juliano/carrental/gui/CreateSpecificationFrame.java
@@ -0,0 +1,98 @@
+package com.juliano.carrental.gui;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import com.juliano.carrental.dao.DaoFactory;
+
+public class CreateSpecificationFrame extends JFrame {
+
+    private static final long serialVersionUID = 3908190433476261982L;
+    private JPanel contentPane;
+    private JTextField nameTextField;
+    private JTextField descriptionTextField;
+
+    /**
+     * Create the frame.
+     */
+    public CreateSpecificationFrame(DaoFactory daoFactory) {
+        setTitle("Cadastro de Especificação");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+
+        JLabel lblNewLabel = new JLabel("Nome:");
+
+        JLabel lblNewLabel_1 = new JLabel("Descrição:");
+
+        nameTextField = new JTextField();
+        nameTextField.setColumns(10);
+
+        descriptionTextField = new JTextField();
+        descriptionTextField.setColumns(10);
+
+        JButton createCategoryBtn = new JButton("Cadastrar");
+
+        createCategoryBtn.addActionListener(
+                new CreateSpecificationBtnActionListener(this, daoFactory, nameTextField,
+                        descriptionTextField));
+
+        GroupLayout gl_contentPane = new GroupLayout(contentPane);
+        gl_contentPane.setHorizontalGroup(
+                gl_contentPane.createParallelGroup(Alignment.LEADING)
+                        .addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+                                        .addGroup(gl_contentPane.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(createCategoryBtn,
+                                                        GroupLayout.PREFERRED_SIZE, 224,
+                                                        GroupLayout.PREFERRED_SIZE))
+                                        .addGroup(gl_contentPane.createSequentialGroup()
+                                                .addGap(25)
+                                                .addGroup(gl_contentPane
+                                                        .createParallelGroup(Alignment.LEADING)
+                                                        .addComponent(lblNewLabel,
+                                                                Alignment.TRAILING)
+                                                        .addComponent(lblNewLabel_1,
+                                                                Alignment.TRAILING))
+                                                .addGap(35)
+                                                .addGroup(gl_contentPane
+                                                        .createParallelGroup(Alignment.LEADING)
+                                                        .addComponent(nameTextField,
+                                                                Alignment.TRAILING,
+                                                                GroupLayout.DEFAULT_SIZE, 235,
+                                                                Short.MAX_VALUE)
+                                                        .addComponent(descriptionTextField,
+                                                                GroupLayout.DEFAULT_SIZE, 187,
+                                                                Short.MAX_VALUE))))
+                                .addGap(92)));
+        gl_contentPane.setVerticalGroup(
+                gl_contentPane.createParallelGroup(Alignment.LEADING)
+                        .addGroup(gl_contentPane.createSequentialGroup()
+                                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(lblNewLabel)
+                                        .addComponent(nameTextField, GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE))
+                                .addGap(32)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(lblNewLabel_1)
+                                        .addComponent(descriptionTextField,
+                                                GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE))
+                                .addGap(47)
+                                .addComponent(createCategoryBtn)
+                                .addGap(68)));
+        contentPane.setLayout(gl_contentPane);
+    }
+}
