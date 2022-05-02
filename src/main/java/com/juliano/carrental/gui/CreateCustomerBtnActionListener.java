055d31f7f761c953064794a449ebc6acdb5ec62c
Juliano Cavalcante Teixeira da Silva <juliano.cavalcante98@gmail.com>
2022-05-02 13:41:03 -0300

    Add GUI for creating Customer

---
+++ src/main/java/com/juliano/carrental/gui/CreateCustomerBtnActionListener.java
@@ -0,0 +1,58 @@
+package com.juliano.carrental.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import org.jdatepicker.JDatePicker;
+
+import com.juliano.carrental.dao.DaoFactory;
+import com.juliano.carrental.model.Customer;
+
+public class CreateCustomerBtnActionListener implements ActionListener {
+    private JFrame frame;
+    private DaoFactory daoFactory;
+    private JTextField nameTextField;
+    private JDatePicker birthDatePicker;
+    private JTextField emailTextField;
+    private JTextField driverLicenseTextField;
+    private JTextField addressTextField;
+    private JTextField phoneNumberTextField;
+
+    public CreateCustomerBtnActionListener(JFrame frame, DaoFactory daoFactory,
+            JTextField nameTextField, JDatePicker birthDatePicker, JTextField emailTextField,
+            JTextField driverLicenseTextField, JTextField addressTextField,
+            JTextField phoneNumberTextField) {
+        this.frame = frame;
+        this.daoFactory = daoFactory;
+        this.nameTextField = nameTextField;
+        this.birthDatePicker = birthDatePicker;
+        this.emailTextField = emailTextField;
+        this.driverLicenseTextField = driverLicenseTextField;
+        this.addressTextField = addressTextField;
+        this.phoneNumberTextField = phoneNumberTextField;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Customer customer = new Customer();
+        customer.setName(this.nameTextField.getText());
+
+        GregorianCalendar calendar = (GregorianCalendar) birthDatePicker.getModel().getValue();
+        Date date = new Date(calendar.getTimeInMillis());
+        customer.setBirthDate(date);
+
+        customer.setEmail(this.emailTextField.getText());
+        customer.setDriverLicense(this.driverLicenseTextField.getText());
+        customer.setAddress(this.addressTextField.getText());
+        customer.setPhoneNumber(this.phoneNumberTextField.getText());
+
+        this.daoFactory.getCustomerDao().save(customer);
+
+    }
+
+}
---
+++ src/main/java/com/juliano/carrental/gui/CreateCustomerFrame.java
@@ -0,0 +1,162 @@
+package com.juliano.carrental.gui;
+
+import java.awt.Dimension;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+
+import org.jdatepicker.JDatePicker;
+
+import com.juliano.carrental.dao.DaoFactory;
+
+public class CreateCustomerFrame extends JFrame {
+    private static final long serialVersionUID = 9137318202706842012L;
+    private DaoFactory daoFactory;
+    private JPanel contentPane;
+    private JTextField nameTextField;
+    private JTextField driverLicenseTextField;
+    private JTextField addressTextField;
+    private JTextField phoneNumberTextField;
+    private JButton createCustomerBtn;
+    private JLabel lblNewLabel_4;
+    private JTextField emailTextField;
+
+    /**
+     * Create the frame.
+     */
+    public CreateCustomerFrame(DaoFactory daoFactory) {
+        this.daoFactory = daoFactory;
+        setTitle("Cadastro de Cliente");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(500, 400));
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+
+        JLabel lblNome = new JLabel("Nome:");
+
+        JLabel lblNewLabel = new JLabel("Data de nascimento:");
+
+        JLabel lblNewLabel_1 = new JLabel("CNH:");
+
+        JLabel lblNewLabel_2 = new JLabel("Endereço:");
+
+        JLabel lblNewLabel_3 = new JLabel("Telefone:");
+
+        nameTextField = new JTextField();
+        nameTextField.setColumns(10);
+
+        driverLicenseTextField = new JTextField();
+        driverLicenseTextField.setColumns(10);
+
+        addressTextField = new JTextField();
+        addressTextField.setColumns(10);
+
+        phoneNumberTextField = new JTextField();
+        phoneNumberTextField.setColumns(10);
+
+        lblNewLabel_4 = new JLabel("Email:");
+
+        JDatePicker birthDatePicker = new JDatePicker();
+
+        emailTextField = new JTextField();
+        emailTextField.setColumns(10);
+
+        createCustomerBtn = new JButton("Cadastrar");
+        createCustomerBtn
+                .addActionListener(new CreateCustomerBtnActionListener(this, daoFactory,
+                        nameTextField, birthDatePicker, emailTextField,
+                        driverLicenseTextField, addressTextField,
+                        phoneNumberTextField));
+
+        GroupLayout gl_contentPane = new GroupLayout(contentPane);
+        gl_contentPane.setHorizontalGroup(
+                gl_contentPane.createParallelGroup(Alignment.TRAILING)
+                        .addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+                                .addGap(30)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+                                        .addComponent(lblNewLabel_1)
+                                        .addComponent(lblNewLabel_2)
+                                        .addComponent(lblNewLabel_3)
+                                        .addComponent(lblNome)
+                                        .addComponent(lblNewLabel)
+                                        .addComponent(lblNewLabel_4))
+                                .addPreferredGap(ComponentPlacement.RELATED)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+                                        .addComponent(birthDatePicker, GroupLayout.DEFAULT_SIZE,
+                                                280,
+                                                Short.MAX_VALUE)
+                                        .addComponent(emailTextField, GroupLayout.DEFAULT_SIZE, 280,
+                                                Short.MAX_VALUE)
+                                        .addGroup(gl_contentPane.createSequentialGroup()
+                                                .addGroup(gl_contentPane
+                                                        .createParallelGroup(Alignment.LEADING)
+                                                        .addComponent(addressTextField, 280, 280,
+                                                                280)
+                                                        .addComponent(phoneNumberTextField, 280,
+                                                                280, 280)
+                                                        .addComponent(createCustomerBtn,
+                                                                Alignment.TRAILING))
+                                                .addPreferredGap(ComponentPlacement.RELATED,
+                                                        GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                        .addComponent(nameTextField, GroupLayout.DEFAULT_SIZE, 280,
+                                                Short.MAX_VALUE)
+                                        .addComponent(driverLicenseTextField,
+                                                GroupLayout.DEFAULT_SIZE, 280,
+                                                Short.MAX_VALUE))
+                                .addGap(89)));
+        gl_contentPane.setVerticalGroup(
+                gl_contentPane.createParallelGroup(Alignment.LEADING)
+                        .addGroup(gl_contentPane.createSequentialGroup()
+                                .addGap(37)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(nameTextField, GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(lblNome))
+                                .addGap(10)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+                                        .addComponent(lblNewLabel)
+                                        .addComponent(birthDatePicker, GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE))
+                                .addGap(18)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(lblNewLabel_4)
+                                        .addComponent(emailTextField, GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE))
+                                .addGap(18)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(lblNewLabel_1)
+                                        .addComponent(driverLicenseTextField,
+                                                GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE))
+                                .addGap(18)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(lblNewLabel_2)
+                                        .addComponent(addressTextField, GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE))
+                                .addGap(18)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(lblNewLabel_3)
+                                        .addComponent(phoneNumberTextField,
+                                                GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE))
+                                .addGap(32)
+                                .addComponent(createCustomerBtn)
+                                .addContainerGap(42, Short.MAX_VALUE)));
+        contentPane.setLayout(gl_contentPane);
+    }
+}
